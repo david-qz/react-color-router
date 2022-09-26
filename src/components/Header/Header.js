a2dc5726d49d238cd7dac562daedfed8afacbaf9
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-09-26 15:39:27 -0700

    Give the colors names

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,8 +6,8 @@ export default function Header() {
   const cssColor = `rgb(${r}, ${g}, ${b})`;
 
   return <header className='Header' style={{ '--current-color' : cssColor }}>
-    <NavLink to="/rgb/95/15/64">color1</NavLink>
-    <NavLink to="/rgb/227/100/20">color2</NavLink>
-    <NavLink to="/rgb/15/76/92">color3</NavLink>
+    <NavLink to="/rgb/95/15/64">purple</NavLink>
+    <NavLink to="/rgb/227/100/20">orange</NavLink>
+    <NavLink to="/rgb/15/76/92">blue</NavLink>
   </header>;
 }
