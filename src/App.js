d761d159e7ec6b81f9cbe2ad18582b187a6dfb83
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-09-26 11:03:31 -0700

    Add router skeleton
    Has a navbar with three colors and routes for root and 404.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { NavLink, Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return <></>;
+  return <>
+    <header>
+      <nav>
+        <ul>
+          <li><NavLink to="/rgb/255/0/0">color1</NavLink></li>
+          <li><NavLink to="/rgb/0/255/0">color2</NavLink></li>
+          <li><NavLink to="/rgb/0/0/255">color3</NavLink></li>
+        </ul>
+      </nav>
+    </header>
+    <Switch>
+      <Route exact path="/">
+        <Redirect to="/rgb/255/0/0" />
+      </Route>
+      <Route path="*">
+        <h1>Not Found</h1>
+      </Route>
+    </Switch>
+  </>;
 }
 
 export default App;
