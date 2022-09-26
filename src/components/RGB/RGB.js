a9233637cfd1e4cfad65a803ffdf7e658058c60c
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-09-26 15:52:45 -0700

    Change text color to optimize background contrast

--- src/components/RGB/RGB.js
+++ src/components/RGB/RGB.js
@@ -2,11 +2,19 @@ import { useParams } from 'react-router-dom';
 import './RGB.css';
 
 export default function RGB() {
-  const color = useParams();
+  const { r, g, b } = useParams();
+
+  const cMax = Math.max(parseInt(r), parseInt(g), parseInt(b)) / 255;
+  const cMin = Math.min(parseInt(r), parseInt(g), parseInt(b)) / 255;
+  const lightness = cMax + cMin / 2;
+
   return <div
     className="RGB"
-    style={{ backgroundColor: `rgb(${color.r}, ${color.g}, ${color.b})` }}
+    style={{
+      backgroundColor: `rgb(${r}, ${g}, ${b})`,
+      color: lightness > 0.45 ? 'black' : 'var(--off-white)'
+    }}
   >
-    {`rgb(${color.r}, ${color.g}, ${color.b})`}
+    {`rgb(${r}, ${g}, ${b})`}
   </div>;
 }
